ed894f60638ce60b9f5ed776c440f59b8b31d415
farhacoder <coder2coders@gmail.com>
2025-03-05 21:51:33 +0530

    learned Get,Post and pust Request

--- back.js
+++ back.js
@@ -1,17 +1,40 @@
 const express=require("express")
 const app=express()
-function sum(n){
-    let ans=0 
-    for(let i=0; i<=n;i++){
-        ans=ans+i
-    }
-    return ans
-        
+var users=[{
+    name:"john",
+    kidney:[{healthy:false},
+    ]
+}]
 
-}
+app.use(express.json());
+
 app.get("/",function(req,res){
-    const n=req.query.n
-    const ans=sum(n)
-    res.send("Hi there"+ans)
+    const johnKidneys=users[0].kidney
+    const noOfKindneys=johnKidneys.length
+    let noOfHealthyKidney=0
+    for(let i=0 ;i<johnKidneys.length;i++)
+        {
+            if(johnKidneys[i].healthy){
+                noOfHealthyKidney+=1
+            }
+        }
+    const unHealthyKidney=noOfKindneys-noOfHealthyKidney
+    res.json({noOfKindneys,noOfHealthyKidney,unHealthyKidney})
+})
+app.post("/",function(req,res){
+    const isHealthy=req.body.isHealthy
+    users[0].kidney.push({
+        healthy:isHealthy
+    })
+    res.json({
+        msg:"done"
+    })
+})
+
+app.put("/", function(req,res){
+    for(let i=0;i<users[0].kidney.length;i++){
+        users[0].kidney[i].healthy=true
+    }
+    res.json({})
 })
 app.listen(3000)
