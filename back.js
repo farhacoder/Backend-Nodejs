da7fdf2eaaaac2551877cf586bcbfb185ff5e220
farhacoder <coder2coders@gmail.com>
2025-03-14 00:15:36 +0530

    Middlewares

--- back.js
+++ back.js
@@ -1,23 +1,41 @@
 const express = require("express");
 const app = express();
 
-app.get("/health-checkup", function (req, res) {
-  const kidneyId = req.query.kidneyId;
+function userMiddleware(req,res,next){
+  
   const username = req.headers.username;
   const password = req.headers.password;
   if (username != "farha" && password != "pass") {
     res.status(400).json({
       msg: "user doest not exit",
     });
-    return;
+    
   }
-  if (kidneyId != 1 && kidneyId != 2) {
-    res.status(400).json({
-      msg: "wrong input",
-    });
-    return;
-  }
-  res.json({ msg: "you are okay" });
-});
+else{
+  next()
+}}
+   function Middleware(req,res,next){
+    const kidneyId = req.query.kidneyId;
+    if (kidneyId != 1 && kidneyId != 2) {
+      res.status(400).json({
+        msg: "wrong input",
+      });
+    }
+     else{ 
+      next()
+     }
+    
+  
+  
+}
+   
+
+app.get("/health-checkup",userMiddleware,Middleware, function (req, res) {
+  res.json({
+    msg:"all set"
+  })
+  
+  })
+ 
 
 app.listen(3000);
