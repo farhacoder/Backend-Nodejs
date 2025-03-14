6b01e00db96054e17c7b3228e1a4d2b2b234cd94
farhacoder <coder2coders@gmail.com>
2025-03-14 20:33:23 +0530

    learnt Zod

--- back.js
+++ back.js
@@ -1,39 +1,15 @@
 const express = require("express");
 const app = express();
+const zod=require("zod")
+const schema=zod.array(zod.number())
+app.use(express.json())
 
-function userMiddleware(req,res,next){
-  
-  const username = req.headers.username;
-  const password = req.headers.password;
-  if (username != "farha" && password != "pass") {
-    res.status(400).json({
-      msg: "user doest not exit",
-    });
-    
-  }
-else{
-  next()
-}}
-   function Middleware(req,res,next){
-    const kidneyId = req.query.kidneyId;
-    if (kidneyId != 1 && kidneyId != 2) {
-      res.status(400).json({
-        msg: "wrong input",
-      });
-    }
-     else{ 
-      next()
-     }
-    
-  
-  
-}
    
 
-app.get("/health-checkup",userMiddleware,Middleware, function (req, res) {
-  res.json({
-    msg:"all set"
-  })
+app.get("/health-checkup", function (req, res) {
+  const kidneys=req.body.kidneys
+  const response=schema.safeParse(kidneys)
+  res.send({response})
   
   })
  
