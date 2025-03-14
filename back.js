15ee52cd09b705d24ef9bec393e180c4a47815e2
farhacoder <coder2coders@gmail.com>
2025-03-14 20:39:12 +0530

    update

--- back.js
+++ back.js
@@ -9,7 +9,14 @@ app.use(express.json())
 app.get("/health-checkup", function (req, res) {
   const kidneys=req.body.kidneys
   const response=schema.safeParse(kidneys)
+  if(!response.success){
+    res.status(411).json({
+      msg:"wrong inputs"
+    })
+  }
+else{
   res.send({response})
+}
   
   })
  
